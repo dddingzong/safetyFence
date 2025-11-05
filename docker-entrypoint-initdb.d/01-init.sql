661100bff7cd8b83dc8305ba99f611a7eb486606
dddingzong <cji513@naver.com>
2025-11-05 16:53:32 +0900

    feat: 배포 권한 관련 수정

--- docker-entrypoint-initdb.d/01-init.sql
+++ docker-entrypoint-initdb.d/01-init.sql
@@ -1,11 +1,13 @@
--- Database initialization script
--- This runs automatically when PostgreSQL container starts for the first time
+-- Only connect if DB exists, otherwise create first
+\set ON_ERROR_STOP on
+DO
+$$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'safetyfence_db') THEN
+      PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE safetyfence_db');
+   END IF;
+END
+$$;
 
--- Connect to the application database
-\c safetyfence_db;
-
--- Create PostGIS extension in the application database
+\connect safetyfence_db;
 CREATE EXTENSION IF NOT EXISTS postgis;
-
--- Verify PostGIS installation
-SELECT PostGIS_version();
